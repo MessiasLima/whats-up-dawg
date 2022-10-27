99e61f14134bb4dbba6723bb2412d497b11f04db
Messias Junior <messiaslima.03@gmail.com>
2022-10-27 16:14:59 +0100

    feat: theme

---
+++ app/src/main/java/io/github/messiasjunior/whatsupdawg/core/ui/theme/Color.kt
@@ -0,0 +1,73 @@
+@file:Suppress("MagicNumber")
+
+package io.github.messiasjunior.whatsupdawg.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * This color palette was created using the
+ * [Material theme builder](https://m3.material.io/theme-builder#/custom)
+ *
+ * A dog picture was used to generate this color scheme.
+ * [Base image](https://www.pinterest.co.uk/pin/134334001373838901/).
+ */
+
+val lightPrimary = Color(0xFF9D4234)
+val lightOnPrimary = Color(0xFFFFFFFF)
+val lightPrimaryContainer = Color(0xFFFFDAD4)
+val lightOnPrimaryContainer = Color(0xFF410000)
+val lightSecondary = Color(0xFF9A4520)
+val lightOnSecondary = Color(0xFFFFFFFF)
+val lightSecondaryContainer = Color(0xFFFFDBCE)
+val lightOnSecondaryContainer = Color(0xFF370E00)
+val lightTertiary = Color(0xFF934B00)
+val lightOnTertiary = Color(0xFFFFFFFF)
+val lightTertiaryContainer = Color(0xFFFFDCC5)
+val lightOnTertiaryContainer = Color(0xFF301400)
+val lightError = Color(0xFFBA1A1A)
+val lightErrorContainer = Color(0xFFFFDAD6)
+val lightOnError = Color(0xFFFFFFFF)
+val lightOnErrorContainer = Color(0xFF410002)
+val lightBackground = Color(0xFFFFFBFF)
+val lightOnBackground = Color(0xFF201A19)
+val lightSurface = Color(0xFFFFFBFF)
+val lightOnSurface = Color(0xFF201A19)
+val lightSurfaceVariant = Color(0xFFF5DDDA)
+val lightOnSurfaceVariant = Color(0xFF534341)
+val lightOutline = Color(0xFF857370)
+val lightInverseOnSurface = Color(0xFFFBEEEC)
+val lightInverseSurface = Color(0xFF362F2E)
+val lightInversePrimary = Color(0xFFFFB4A8)
+val lightSurfaceTint = Color(0xFF9D4234)
+val lightOutlineVariant = Color(0xFFD8C2BE)
+val lightScrim = Color(0xFF000000)
+
+val darkPrimary = Color(0xFFFFB4A8)
+val darkOnPrimary = Color(0xFF5F140C)
+val darkPrimaryContainer = Color(0xFF7E2B20)
+val darkOnPrimaryContainer = Color(0xFFFFDAD4)
+val darkSecondary = Color(0xFFFFB599)
+val darkOnSecondary = Color(0xFF5A1C00)
+val darkSecondaryContainer = Color(0xFF7B2F0A)
+val darkOnSecondaryContainer = Color(0xFFFFDBCE)
+val darkTertiary = Color(0xFFFFB782)
+val darkOnTertiary = Color(0xFF4F2500)
+val darkTertiaryContainer = Color(0xFF703800)
+val darkOnTertiaryContainer = Color(0xFFFFDCC5)
+val darkError = Color(0xFFFFB4AB)
+val darkErrorContainer = Color(0xFF93000A)
+val darkOnError = Color(0xFF690005)
+val darkOnErrorContainer = Color(0xFFFFDAD6)
+val darkBackground = Color(0xFF201A19)
+val darkOnBackground = Color(0xFFEDE0DD)
+val darkSurface = Color(0xFF201A19)
+val darkOnSurface = Color(0xFFEDE0DD)
+val darkSurfaceVariant = Color(0xFF534341)
+val darkOnSurfaceVariant = Color(0xFFD8C2BE)
+val darkOutline = Color(0xFFA08C89)
+val darkInverseOnSurface = Color(0xFF201A19)
+val darkInverseSurface = Color(0xFFEDE0DD)
+val darkInversePrimary = Color(0xFF9D4234)
+val darkSurfaceTint = Color(0xFFFFB4A8)
+val darkOutlineVariant = Color(0xFF534341)
+val darkScrim = Color(0xFF000000)
